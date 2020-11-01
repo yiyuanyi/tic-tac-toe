ad90786bff10c55e343c7a14dbb6999701ddf3c8
Matt H <59845559+yiyuanyi@users.noreply.github.com>
2020-11-01 15:51:01 +0000

    add pseudocode.

---
+++ script.js
@@ -0,0 +1,8 @@
+const gameBoard = (() => {
+    const tiles = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X',];
+    //render gameboard to HTML on window load
+}
+
+const player = ((name) => {
+    //set player name to name value from input
+}
