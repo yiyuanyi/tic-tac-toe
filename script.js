f3d75e7418f7af8d83be70bffabc9f6518aceca5
Matt H <59845559+yiyuanyi@users.noreply.github.com>
2020-11-04 19:31:22 +0000

    add renderGameBoard function. add selector for all times. add loop to loop through tiles and console.log the corresponding counter.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const gameBoard = (() => {
-    const tiles = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X',];
-    //render gameboard to HTML on window load
-}
+    const counters = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
+    return { counters };
+})();
+
+const displayController = (() => {
+    
+})();
 
 const player = ((name) => {
     //set player name to name value from input
+})
+
+function renderGameBoard() {
+    const tiles = document.querySelectorAll('.tile');
+    console.log(tiles.length);
+    for (let i = 0; i < tiles.length; i++) {
+        let placedCounter = gameBoard.counters[i];
+        console.log(gameBoard.counters[i]);
+    }
 }
