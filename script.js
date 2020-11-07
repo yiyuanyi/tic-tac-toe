dc2b497abacf6d14593fb36d7cada084315d7509
Matt H <59845559+yiyuanyi@users.noreply.github.com>
2020-11-07 11:41:22 +0000

    displayController module within gameboard. playGame factory function. May have to rework these as the nesting is likely wrong.

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 const gameBoard = (() => {
-    const counters = ['X', 'O', 'O', 'X', 'X', 'O', 'X', 'O', 'X'];
-    return { counters };
-})();
+    const markers = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
 
-const displayController = (() => {
-    //render enter player names, play computer, reset button etc.
-})();
+    const displayController = (() => {
 
-const player = ((name) => {
-    //set player name to name value from input
-})
+        //empty results array
 
-function renderGameBoard() {
-    const tiles = document.querySelectorAll('.tile');
-    for (let i = 0; i < tiles.length; i++) {
+        const renderButtons = () => {
+            //render new game button
+            //on click render inputs for player names
+        }
         
+        const player = ((name) => {
+            //set player name to name value from input
+            //set player1 to X and player 2 to O for markers
+        })
+
+        return {
+            renderButtons,
+            player,
+        }
+    })();
+
+    const playGame = () => {
+        const renderMarker = (e) => {
+            if (!e.target.hasChildNodes()) {
+                const para = document.createElement('p');
+                para.innerHTML = 'X';
+                para.classList.add('marker');
+                e.target.appendChild(para);
+            }
+        }
+    
+        const placeMarker = () => {
+            const board = document.querySelector('#board');
+            board.addEventListener('click', renderMarker);
+        }
+
+        placeMarker();
     }
-}
 
-    const board = document.querySelector('#board');
-    board.addEventListener('click', function(e) {
-        const counterLocation = e.target;
-        const para = document.createElement('p');
-        para.innerHTML = 'X';
-        para.classList.add('counter');
-        e.target.appendChild(para);
-    })
+    return {
+        displayController,
+        playGame,
+    };
+})();
 
+gameBoard.playGame();
