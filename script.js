edab3e8ea6f3854c0fc76a25531f3eac11c12a28
Matt H <59845559+yiyuanyi@users.noreply.github.com>
2020-11-07 11:44:45 +0000

    move displayController out of gameBoard module. Move playGame factory function out of gameBoard.

--- script.js
+++ script.js
@@ -1,48 +1,43 @@
 const gameBoard = (() => {
     const markers = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
 
-    const displayController = (() => {
-
-        //empty results array
-
-        const renderButtons = () => {
-            //render new game button
-            //on click render inputs for player names
-        }
-        
-        const player = ((name) => {
-            //set player name to name value from input
-            //set player1 to X and player 2 to O for markers
-        })
-
-        return {
-            renderButtons,
-            player,
-        }
-    })();
-
-    const playGame = () => {
-        const renderMarker = (e) => {
-            if (!e.target.hasChildNodes()) {
-                const para = document.createElement('p');
-                para.innerHTML = 'X';
-                para.classList.add('marker');
-                e.target.appendChild(para);
-            }
-        }
-    
-        const placeMarker = () => {
-            const board = document.querySelector('#board');
-            board.addEventListener('click', renderMarker);
-        }
-
-        placeMarker();
-    }
-
-    return {
-        displayController,
-        playGame,
-    };
 })();
 
-gameBoard.playGame();
+const displayController = (() => {
+
+    //empty results array
+
+    const renderButtons = () => {
+        //render new game button
+        //on click render inputs for player names
+    }
+    
+    const player = ((name) => {
+        //set player name to name value from input
+        //set player1 to X and player 2 to O for markers
+    })
+
+    return {
+        renderButtons,
+        player,
+    }
+})();
+
+const playGame = () => {
+    const renderMarker = (e) => {
+        if (!e.target.hasChildNodes()) {
+            const para = document.createElement('p');
+            para.innerHTML = 'X';
+            para.classList.add('marker');
+            e.target.appendChild(para);
+        }
+    }
+
+    const placeMarker = () => {
+        const board = document.querySelector('#board');
+        board.addEventListener('click', renderMarker);
+    }
+    placeMarker();
+}
+
+window.addEventListener('load', playGame());
